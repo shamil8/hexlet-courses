f70fb3ea23940229c7b740cadb5783d8a2fc01ca
Shamil Kurbonov <37189293+shamil8@users.noreply.github.com>
2020-07-21 14:55:33 +0700

    More tasks

--- js-arrays.js
+++ js-arrays.js
@@ -133,3 +133,72 @@ const money1 = [
   'eur 10', 'usd 1', 'usd 10', 'rub 50', 'usd 5',
 ];
 console.log(getTotalAmount(money1, 'usd')) // 16
+
+// step 26
+const buildDefinitionList = (arr) => {
+  if(!arr.length) {
+    return ''
+  }
+
+  const parts = []
+
+  for(const els of arr) {
+    const name = els[0]
+    const desciption = els[1]
+
+    parts.push(`<dt>${name}</dt><dd>${desciption}</dd>`)
+  }
+
+  const innerValue = parts.join('')
+
+  const result = `<dl>${innerValue}</dl>`
+
+  return result
+}
+
+const definitions = [
+  ['Блямба', 'Выпуклость, утолщения на поверхности чего-либо'],
+  ['Бобр', 'Животное из отряда грызунов'],
+]
+
+// console.log(buildDefinitionList(definitions) )
+
+// step 28
+const makeCensored = (sentence, censores) => {
+  const newSentences = []
+
+  const sentences = sentence.split(' ')
+
+  for(let i = 0; i < sentences.length; i++) {
+    let found = ''
+
+    for(const censore of censores) {
+      if(sentences[i] === censore) {
+        found = censore
+      }
+    }
+
+    newSentences.push(found ? '$#%!' : sentences[i])
+  }
+
+  return newSentences.join(' ')
+}
+
+// console.log(makeCensored('When you play the game of thrones, you win or you die', ['die', 'play']))
+
+// step 30
+const getSameCount = (firstArr, secondArr) => {
+  const newArr = []
+
+  for(let i = 0; i < firstArr.length; i++) {
+    for(let j = 0; j < secondArr.length; j++) {
+      if(firstArr[i] === secondArr[j]) {
+        newArr.push(firstArr[i])
+      }
+    }
+  }
+
+  return Array.from(new Set(newArr)).length
+}
+
+// console.log(getSameCount([1, 3, 2, 2], [3, 1, 1, 2, 5])) // 3
