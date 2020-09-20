fda501c12dd9964409ce6640958628ce922732de
Shamil Dev <qurbonovshamil@gmail.com>
2020-09-20 23:35:03 +0700

    Added new tasks

--- js-objects.js
+++ js-objects.js
@@ -156,3 +156,115 @@ const users = [
 ];
 
 
+//Step modifications
+/**
+ Реализуйте и экспортируйте по умолчанию функцию, которая
+ "нормализует" данные переданного урока. То есть приводит их к определенному виду.
+ */
+const lesson = {
+  name: 'ДеструКТУРИЗАЦИЯ',
+  description: 'каК удивитЬ колек',
+};
+
+const normalize = (obj) => {
+  obj.name = obj.name.length
+      ? obj.name.length && obj.name.slice(0, 1).toUpperCase() + obj.name.slice(1, obj.name.length).toLowerCase()
+      : ''
+  obj.description = obj.description.toLowerCase()
+}
+
+normalize(lesson)
+// console.log(lesson)
+
+//Step references
+/**
+ * Реализуйте и экспортируйте по умолчанию функцию, которая сравнивает объекты по совпадению данных,
+ * а не по ссылкам. Эта функция принимает на вход две компании и возвращает true
+ * если их структура одинаковая и false в обратном случае. Проверка должна проходить по свойствам name, state, website.
+ */
+const company1 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+const company2 = { name: 'Hexlet', state: 'published', website: 'https://hexlet.io' };
+
+const is = (obj1, obj2) => {
+  const obj1Keys = Object.keys(obj1)
+  let counter = 0
+
+  obj1Keys.forEach(key => obj2[key] === obj1[key] && counter++)
+
+  return counter === obj1Keys.length
+}
+
+// console.log(is(company1, company2)) // true
+
+// Step property-existence
+/**
+ * Реализуйте и экспортируйте по умолчанию функцию,
+ * которая считает количество слов в предложении и возвращает объект в котором свойства
+ * это слова (приведенные к нижнему регистру), а значения — это то сколько раз слово встретилось
+ * в предложении. Слова в предложении могут находиться в разных регистрах.
+ * Перед подсчетом их нужно приводить в нижний регистр, чтобы не пропускались дубли.
+ */
+
+
+const text1 = 'нужно приводить в нижний регистр, чтобы не пропускались дубли'
+
+const countWords = text => {
+  const countWords = {}
+
+  text.length && text
+      .toLowerCase()
+      .split(' ')
+      .forEach(item => countWords.hasOwnProperty(item)
+          ? countWords[item] += 1
+          : countWords[item] = 1
+      )
+
+  return countWords
+}
+
+// console.log(countWords(text1))
+
+// example object
+const course = { name: 'JS: React', slug: 'js-react' }
+const entries = Object.entries(course)
+
+for (const [key, value] of entries) {
+  // console.log(key, value)    // name JS: React
+}
+// Step property-existence
+/**
+ * Реализуйте и экспортируйте по умолчанию функцию, которая извлекает из объекта любой
+ * глубины вложенности значение по указанным ключам.
+ * Параметры:
+  Исходный объект
+    Цепочка ключей (массив), по которой ведётся поиск значения
+    В случае, когда добраться до значения невозможно, возвращается null.
+ */
+// TODO:: You'll need to think about it!
+const data1 = {
+  user: 'ubuntu',
+  hosts: {
+    0: {
+      name: 'web1',
+    },
+    1: {
+      name: 'web2',
+      null: 3,
+      active: false,
+    },
+  },
+}
+const get = (obj, keys) => {
+  keys.forEach(key => {
+    obj.hasOwnProperty(key)
+  })
+
+  console.log(obj)
+}
+get(data1, ['undefined']); // null
+get(data1, ['user']); // 'ubuntu'
+get(data1, ['user', 'ubuntu']); // null
+get(data1, ['hosts', 1, 'name']); // 'web2'
+get(data1, ['hosts', 0]); // { name: 'web1' }
+get(data1, ['hosts', 1, null]); // 3
+get(data1, ['hosts', 1, 'active']); // false
